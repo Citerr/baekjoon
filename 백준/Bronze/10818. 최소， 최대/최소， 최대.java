a16f7bc54c3ba333fc76b8c6f2033a15c2d281dc
Citerr <94782823+Citerr@users.noreply.github.com>
2024-03-08 23:45:04 +0900

    [Bronze III] Title: 최소, 최대, Time: 1348 ms, Memory: 105364 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; N > i; i++) {
+		    arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		bw.write(arr[0] + " " + arr[N - 1] + "\n");
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+}
