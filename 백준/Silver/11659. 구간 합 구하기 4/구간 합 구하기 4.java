3083f260c6108eef6af4d3b2872bcaf482a470f9
Citerr <94782823+Citerr@users.noreply.github.com>
2024-05-14 14:08:28 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2616 ms, Memory: 257432 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+
+    int[] arr = new int[N + 1];
+    for (int i = 1; i <= N; i++) {
+      arr[i] = arr[i - 1] + sc.nextInt();
+    }
+
+    for (int i = 0; i < M; i++) {
+      int stt = sc.nextInt();
+      int end = sc.nextInt();
+
+      System.out.println(arr[end] - arr[stt - 1]);
+    }
+    
+    sc.close();
+  }
+}
