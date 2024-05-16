8669bc295697a31d39b664d727e9ecdbdfa2961a
Citerr <94782823+Citerr@users.noreply.github.com>
2024-05-16 14:04:41 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 132 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  static int[] cases = new int[1001];
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    int N = Integer.parseInt(br.readLine());
+    cases[1] = 1; 
+    cases[2] = 3; 
+    cases[3] = 5;
+
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(tiling(N) + "");
+    bw.flush();
+    bw.close();
+  }
+
+  static int tiling(int N) {
+    if (cases[N] != 0) return cases[N];
+    else 
+      cases[N] =( (N%2 == 0) ? 2 * tiling(N - 1) + 1 : 2 * tiling(N - 1) - 1 ) % 10007;
+    return cases[N];
+  }
+}
