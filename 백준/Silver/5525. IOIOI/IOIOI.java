13b3640d0bdd19ae336053f3f76972ba73582e79
Citerr <94782823+Citerr@users.noreply.github.com>
2024-07-07 19:16:05 +0900

    [Silver I] Title: IOIOI, Time: 292 ms, Memory: 22608 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -4,26 +4,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 //import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
-    StringBuilder sb = new StringBuilder();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
     int M = Integer.parseInt(br.readLine());
     String str = new String(br.readLine());
-    for (int i = 0; i < N; i++) {
-      sb.append("IO");
+    
+    Queue<Integer> blocckQ = new LinkedList<>();
+    int idx = str.indexOf("IOI");
+    while (idx != -1) {
+      int block = 1;
+      for (int i = idx + 3; i < M; i++) {
+        if ( (i - idx)%2 == 0 &&  str.charAt(i) == 'I') block++;
+        else if ( (i - idx)%2 == 1 && str.charAt(i) == 'O') continue;
+        else break;
+        //System.out.println(block + " " + i);
+      }
+      blocckQ.offer(block);
+      idx = str.indexOf("IOI", idx + block*2 + 1);
+      //System.out.println(block + " " + idx);
     }
-    sb.append("I");
-    String Pn = new String( sb.toString() );
+
+    /*System.out.print("Q: ");
+    for (Integer integer : blocckQ) {
+      System.out.print(" " + integer);
+    }
+    System.out.println();*/
 
     int RES = 0;
-    int idx = 0;
-    while ( true ) {
-      idx = str.indexOf(Pn, idx);
-      if (idx++ != -1) RES++;
-      else break;
+    while ( !blocckQ.isEmpty() ) {
+      int temp = blocckQ.poll() - (N - 1);
+      RES += temp > 0 ? temp : 0;
+      //System.out.println( RES );
     }
 
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -32,3 +48,7 @@ public class Main {
     bw.close();
   }
 }
+
+//가장 큰 덩어리 하나 찾고 그 크기를 줄이는 방법
+//N = 2M + 1
+//10101..로 되어 있는 덩어리의 크기를 알면 개수도 알 수 있다...
